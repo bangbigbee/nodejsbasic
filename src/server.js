8fefb13f8ece0daaece1ee17c1e807e803893ff3
bangdtbk <bangdtbk@gmail.com>
2022-07-19 17:16:48 +0700

    staticfile

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 //const path = require('path');
-import configViewEngine from './configs/ViewEngine'
+import configViewEngine from './configs/ViewEngine';
+require('dotenv').config();
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+//console.log('>>>check port:', port)
 
 configViewEngine(app);
 
