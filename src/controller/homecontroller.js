2331468b6c57dafe222d932dc1e5b8dadf2d05c7
bangdtbk <bangdtbk@gmail.com>
2022-07-22 00:50:55 +0700

    createNav+loopEJS

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -1,6 +1,26 @@
+import connection from '../configs/connectDB';
 let getHomepage = (req, res) => {
     //logic
-    res.render('index.ejs');
+    // simple query
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+
+            });
+
+            return res.render('index.ejs', { dataUser: data, test: 'abc string test' });
+        });
+    //console.log('>>> check data: ', typeof (data), JSON.stringify(data));
+
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 //const path = require('path');
 import configViewEngine from './configs/ViewEngine';
 import initWebRoute from './route/web';
+// import connection from './configs/connectDB';
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000;
