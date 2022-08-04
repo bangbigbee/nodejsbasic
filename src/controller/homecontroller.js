71f61cb7aac128ed1081abac555a152dca83b4b0
bangdtbk <bangdtbk@gmail.com>
2022-08-04 15:54:47 +0700

    finish CRUD

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -1,3 +1,5 @@
+//import PoolConnection from 'mysql2/typings/mysql/lib/PoolConnection';
+import e from 'express';
 import pool from '../configs/connectDB';
 let getHomepage = async (req, res) => {
     //logic
@@ -21,6 +23,25 @@ let createNewUser = async (req, res) => {
         [firstName, lastName, email, address]);
     return res.redirect('/')
 }
+
+let deleteUser = async function (_req, res) {
+    let userId = _req.body.userId;
+    await pool.execute('DELETE from users where id = ? ', [userId])
+    return res.redirect('/');
+}
+
+let getEditPage = async (_req, res) => {
+    let id = _req.params.id;
+    let [user] = await pool.execute(`select * from users where id = ?`, [id])
+    //return res.send(JSON.stringify(user))
+    return res.render('update.ejs', { dataUser: user[0] });
+}
+
+let postUpdateUser = async (_req, res) => {
+    let { firstName, lastName, email, address, id } = _req.body;
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?', [firstName, lastName, email, address, id]);
+    return res.redirect('/')
+}
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,9 +6,12 @@ const initWebRoute = (app) => {
     //get muốn lấy thông tin về (Read in CRUD)
     //post muốn thêm thông tin vào dtb (Create in Create-Read-Update-Delete)
     router.get('/', homeController.getHomepage);
-    router.get('/detail/user/:id', homeController.getDetailPage)
-    router.post('/create-new-user', homeController.createNewUser)
-    router.get('/about', (req, res) => {
+    router.get('/detail/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser)
+    router.get('/about', (_req, res) => {
         res.send(`I'm BigBee`)
     })
     return app.use('/', router)
